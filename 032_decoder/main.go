c3d2cf1d5f9a1e51d592910557d28365609fe4d9
abhishek <abhisheksathala296@gmail.com>
2026-02-08 22:37:06 +0530

    add

--- 032_decoder/main.go
+++ 032_decoder/main.go
@@ -7,14 +7,19 @@ import (
 	"time"
 )
 
-func writeJSON(w http.ResponseWriter, r *http.Request) {
-
+func writeJSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("content-Type", "application/json")
+	w.WriteHeader(status)
+	_ = json.NewEncoder(w).Encode(data)
 }
 
 func testhandler(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-
+		writeJSON(w, http.StatusMethodNotAllowed, map[string]any{
+			"ok":    false,
+			"error": "only post is allowed man ",
+		})
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -27,7 +32,7 @@ func testhandler(w http.ResponseWriter, r *http.Request) {
 		"dateandtime": time.Now().UTC(),
 	}
 
-	_ = json.NewEncoder().Encode()
+	_ = json.NewEncoder(w).Encode(res)
 
 }
 
