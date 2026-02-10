359297f85b95f61537b4e13d0a8e1f604478f572
abhishek <abhisheksathala296@gmail.com>
2026-02-11 00:21:23 +0530

    add

--- 00_gin/internal/db/mongo.go
+++ 00_gin/internal/db/mongo.go
@@ -1,1 +1,15 @@
 package db
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Connect(cfg config.config) (*mongo.Client, *mongo.Database, error) {
+	// prevent your app from freezing man
+	cxt, cancle := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancle()
+
+}
